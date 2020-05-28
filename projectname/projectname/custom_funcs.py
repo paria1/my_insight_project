228a598eb988fa6603bd103c1e03dc763a787c7b
Paria Avij <paria.avij@gmail.com>
2020-05-28 13:17:15 -0700

    add new files

---
+++ projectname/projectname/custom_funcs.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 12:55:34 2020
+
+@author: paria
+"""
+
---
+++ projectname/setup.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 12:57:21 2020
+
+@author: paria
+"""
+
