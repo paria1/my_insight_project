036d63addf2753f8cec26b2f34c7d624f20be3ab
bedayat <paria.avij@gmail.com>
2020-06-17 00:21:30 -0700

    4

--- Workshop_Shopee-master/ds_project_shopee.py
+++ Workshop_Shopee-master/ds_project_shopee.py
@@ -7,44 +7,65 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 import time
+import json
+from joblib import dump, load
 
 #import the data
 data = pd.read_csv("Data Clean.csv")
+data1 = pd.read_csv("sample_data.csv")
 # image = Image.open("house.png")
 image = Image.open("online-shopping.jpg")
 
 st.title("Welcome to the PITeller")
 st.image(image, use_column_width=True)
 
+## import some data
+# min_dict = json.loads('min_dict.json')
+# max_dict = json.loads('max_dict.json')
+# st.write("max_dict", max_dict)
+
 #checking the data
 st.write("Do you want to learn more about your customer shopping behavior? PITeller has the answer for you!")
-st.markdown('<style>h1{color: red;}</style>', "Don't you believe that? Let's try and see!")
-check_data = st.checkbox("See the simple data")
+st.markdown( "Don't you believe that? Let's try and see!")
+check_data = st.checkbox("Check the sample data")
 if check_data:
-    st.write(data.head())
-st.write("Now let's find out how much the prices when we choosing some parameters.")
+    # st.write(data.head())
+    st.write(data1.head())
+st.write("Now let's find about  Customer Purchase Intention!")
+
+
+
 
 #input the numbers
-sqft_liv = st.slider("What is your square feet of living room?",int(data.sqft_living.min()),int(data.sqft_living.max()),int(data.sqft_living.mean()) )
-bath     = st.slider("How many bathrooms?",int(data.bathrooms.min()),int(data.bathrooms.max()),int(data.bathrooms.mean()) )
-bed      = st.slider("How many bedrooms?",int(data.bedrooms.min()),int(data.bedrooms.max()),int(data.bedrooms.mean()) )
-floor    = st.slider("How many floor do you want?",int(data.floors.min()),int(data.floors.max()),int(data.floors.mean()) )
+pagevalues = st.slider("What is the page value from google analytics?"
+    ,int(data1.PageValues.min())
+    ,int(data1.PageValues.max())
+    ,int(data1.PageValues.mean()) )
+
+exitrates = st.slider("What is the exit rates from google analytics?"
+    ,float(data1.ExitRates.min())
+    ,float(data1.ExitRates.max())
+    ,float(data1.ExitRates.mean()) )
+
+
+
+
 
 #splitting your data
-X = data.drop('price', axis = 1)
-y = data['price']
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.2, random_state=45)
 
-#modelling step
-#import your model
-model=LinearRegression()
-#fitting and predict your model
-model.fit(X_train, y_train)
-model.predict(X_test)
-errors = np.sqrt(mean_squared_error(y_test,model.predict(X_test)))
-predictions = model.predict([[sqft_liv,bath,bed,floor]])[0]
+
+## My Ml Models
+clf_rf_f = load('clf_rf_f.joblib')
+predictions = clf_rf_f.predict([[pagevalues,exitrates]])[0]
+
 
 #checking prediction house price
 if st.button("Run me!"):
-    st.header("Your house prices prediction is USD {}".format(int(predictions)))
-    st.subheader("Your range of prediction is USD {} - USD {}".format(int(predictions-errors),int(predictions+errors) ))
+    st.header("The Customer Most Probably ")
+    if predictions ==1:
+        st.header("BUYS")
+    if predictions ==0:
+        st.header("DOES NOT BUY")
+    
+    #.format(int(predictions)))
+    #st.subheader("Your range of prediction is USD {} - USD {}".format(int(predictions-errors),int(predictions+errors) ))
