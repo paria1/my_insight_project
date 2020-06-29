d4dcdd11cf632b7af503206f7d314b08ad87e663
bedayat <paria.avij@gmail.com>
2020-06-29 00:55:04 -0700

    cleaned the folder

--- projectname/projectname/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 28 12:55:34 2020
-
-@author: paria
-"""
-
--- projectname/setup.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May 28 12:57:21 2020
-
-@author: paria
-"""
-
