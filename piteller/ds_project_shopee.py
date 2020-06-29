9d83806270359cd759da64c3331874a5afd4de09
bedayat <paria.avij@gmail.com>
2020-06-29 00:42:41 -0700

    touch up

--- piteller/ds_project_shopee.py
+++ piteller/ds_project_shopee.py
@@ -35,12 +35,23 @@ st.write("Now let's find about  Customer Purchase Intention!")
 
 #input the numbers
 
-pagevalues  = st.slider('What is the page value from google analytics?', float(data.pagevalues.min()), float(data.pagevalues.max()), float(data.pagevalues.mean()))
-exitrates  = st.slider('What is the exit rates from google analytics?', float(data.exitrates.min()), float(data.exitrates.max()), float(data.exitrates.mean()))
-administrative_duration  = st.slider('administrative_duration', float(data.administrative_duration.min()), float(data.administrative_duration.max()), float(data.administrative_duration.mean()))
-productrelated_duration  = st.slider('productrelated_duration', float(data.productrelated_duration.min()), float(data.productrelated_duration.max()), float(data.productrelated_duration.mean()))
-bouncerates  = st.slider('bouncerates', float(data.bouncerates.min()), float(data.bouncerates.max()), float(data.bouncerates.mean()))
-productrelated  = st.slider('productrelated', float(data.productrelated.min()), float(data.productrelated.max()), float(data.productrelated.mean()))
+# pagevalues  = st.slider('What is the page value from google analytics?', float(data.pagevalues.min()), float(data.pagevalues.max()), float(data.pagevalues.mean()))
+# exitrates  = st.slider('What is the exit rates from google analytics?', float(data.exitrates.min()), float(data.exitrates.max()), float(data.exitrates.mean()))
+# administrative_duration  = st.slider('administrative_duration', float(data.administrative_duration.min()), float(data.administrative_duration.max()), float(data.administrative_duration.mean()))
+# productrelated_duration  = st.slider('productrelated_duration', float(data.productrelated_duration.min()), float(data.productrelated_duration.max()), float(data.productrelated_duration.mean()))
+# bouncerates  = st.slider('bouncerates', float(data.bouncerates.min()), float(data.bouncerates.max()), float(data.bouncerates.mean()))
+# productrelated  = st.slider('productrelated', float(data.productrelated.min()), float(data.productrelated.max()), float(data.productrelated.mean()))
+# month  = st.selectbox('month', ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'] )
+# visitortype  = st.selectbox('visitortype', ['Returning_Visitor', 'New_Visitor', 'Unkown'] )
+
+
+
+pagevalues  = st.slider('What is the page value from google analytics?', float(data.pagevalues.min()), 300., 70.)
+exitrates  = st.slider('What is the exit rates from google analytics?', float(data.exitrates.min()), float(data.exitrates.max()), .07)
+administrative_duration  = st.slider('administrative_duration', float(data.administrative_duration.min()), float(600.), float(100.))
+productrelated_duration  = st.slider('productrelated_duration', float(0), float(3600), float(120))
+bouncerates  = st.slider('bouncerates', float(data.bouncerates.min()), float(data.bouncerates.max()), .1)
+productrelated  = st.slider('productrelated', float(data.productrelated.min()), 200., float(30))
 month  = st.selectbox('month', ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'] )
 visitortype  = st.selectbox('visitortype', ['Returning_Visitor', 'New_Visitor', 'Unkown'] )
 
@@ -83,14 +94,44 @@ elif visitortype == 'Unkown':
 ## My Ml Models
 clf_rf_f = load('clf_rf_f.joblib')
 predictions = clf_rf_f.predict([[pagevalues,exitrates,administrative_duration,productrelated_duration,bouncerates,productrelated,month_Jan,month_Feb,month_Mar,month_Apr,month_May,month_Jun,month_Jul,month_Aug,month_Sep,month_Oct,month_Nov,month_Dec,visitortype_New_Visitor,visitortype_Returning_Visitor,visitortype_Unkown]])[0]
+prob = clf_rf_f.predict_proba([[pagevalues,exitrates,administrative_duration,productrelated_duration,bouncerates,productrelated,month_Jan,month_Feb,month_Mar,month_Apr,month_May,month_Jun,month_Jul,month_Aug,month_Sep,month_Oct,month_Nov,month_Dec,visitortype_New_Visitor,visitortype_Returning_Visitor,visitortype_Unkown]])
 
 
+import PIL
+from PIL import Image
+percent = round(prob[:,1][0],2)*100  # Percent for gauge
+output_file_name = 'new_gauge.png'
+x = 825
+y = 825
+loc = (x, y)
+percent = percent / 100
+rotation = 180 * percent  # 180 degrees because the gauge is half a circle
+rotation = 90 - rotation  # Factor in the needle graphic pointing to 50 (90 degrees)
+dial = Image.open('needle.png')
+dial = dial.rotate(rotation, resample=PIL.Image.BICUBIC, center=loc)  # Rotate needle
+
+gauge = Image.open('gauge.png')
+gauge.paste(dial, mask=dial)  # Paste needle onto gauge
+gauge.save(output_file_name)
+
+
+
+buy_string = "Prediction Probability = " +  str(int(round(prob[:,1][0],2)*100)) + '%'
+not_buy_string = "Prediction Probability = " +  str(int(round(prob[:,0][0],2)*100)) + '%'
+# print(prob[:,0])
 #checking prediction 
 if st.button("Run me!"):
     st.header("The Customer Most Probably ")
     if predictions ==1:
         st.header("BUYS")
+        st.header(buy_string)
     if predictions ==0:
         st.header("DOES NOT BUY")
+        st.header(not_buy_string)
+        
+image = Image.open('new_gauge.png')
+
+#st.title("Welcome to the PITeller")
+st.image(image, width=300)
 
 
